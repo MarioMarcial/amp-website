c1cb2129ad8219329947ab21753babdc101c0c51
Mario Marcial <mariomarciallp@gmail.com>
2025-06-03 12:04:57 -0600

    Testimonials design fixed

--- src/components/testimonials/TestimonialsGrid.tsx
+++ src/components/testimonials/TestimonialsGrid.tsx
@@ -1,31 +1,60 @@
+import { ImQuotesRight } from 'react-icons/im';
 import { testimonialsData } from '../../data';
 
 export const TestimonialsGrid = () => {
   return (
     <>
-      <div className="grid space-y-5 md:grid-cols-3 md:gap-x-4 md:justify-center md:space-y-0 xl:gap-x-6">
+      <div className="grid gap-5 md:justify-center lg:grid-cols-3">
         {testimonialsData.map((testimonial) => (
           <div
             key={testimonial.id}
-            className="w-full p-8 space-y-5 rounded-xl bg-gray-dark border border-gray-800"
+            className="w-full h-full p-8 space-y-6 rounded-xl bg-gray-dark border border-gray-800 lg:flex lg:flex-col lg:justify-between lg:space-y-3"
           >
-            <div className="w-26 xl:w-30 mx-auto">
-              <img
-                src={`${import.meta.env.BASE_URL}images/${testimonial.img}`}
-                className="rounded-full object-cover object-center pointer-events-none"
-                alt={`${testimonial.name} imagen`}
-              />
+            {/* CardHeader */}
+            <div className="flex justify-start items-center gap-4 lg:flex-col">
+              <div className="lg:space-y-2">
+                <img
+                  src={`${import.meta.env.BASE_URL}images/${testimonial.img}`}
+                  className="rounded-full object-cover object-center pointer-events-none w-15 lg:w-20 lg:mx-auto"
+                  alt={`${testimonial.name} imagen`}
+                  loading="lazy"
+                />
+                <img
+                  className="hidden w-25 grayscale-100 brightness-150 lg:mx-auto lg:block"
+                  src={`${import.meta.env.BASE_URL}images/estrellas.png`}
+                  alt="calificación imagen"
+                  loading="lazy"
+                />
+              </div>
+              <div className="lg:hidden">
+                <p className="font-semibold text-primary/90">
+                  {testimonial.name}
+                </p>
+                <p className="text-gray-400">
+                  Estudiante de {testimonial.instrument}
+                </p>
+              </div>
             </div>
-            <div className="text-center">
-              <img
-                src={`${import.meta.env.BASE_URL}images/estrellas.png`}
-                className="w-1/3 mx-auto"
-                alt="calificación imagen"
-              />
-              <p className="text-gray-300">{testimonial.review}</p>
+
+            {/* Card Body */}
+            <div className="relative space-y-2">
+              <div className="w-25 grayscale-100 brightness-150 lg:mx-auto lg:hidden">
+                <img
+                  className="lg:hidden"
+                  src={`${import.meta.env.BASE_URL}images/estrellas.png`}
+                  alt="calificación imagen"
+                  loading="lazy"
+                />
+              </div>
+              <p className="text-gray-300 lg:text-center">
+                {testimonial.review}
+              </p>
+              <ImQuotesRight className="w-10 h-10 ml-auto absolute -bottom-7 -right-3 fill-primary/8 lg:hidden" />
             </div>
-            <div className="text-center">
-              <p className="text-primary/90 font-semibold">
+
+            {/* Card Footer */}
+            <div className="hidden lg:block lg:text-center">
+              <p className="font-semibold text-primary/90">
                 {testimonial.name}
               </p>
               <p className="text-gray-400">
