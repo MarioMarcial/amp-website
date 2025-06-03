5983b07a01d9d6dd1790ea8df2c913a37ab3a30e
Mario Marcial <mariomarciallp@gmail.com>
2025-06-03 12:07:10 -0600

    Sizes added to the container component

--- src/components/ui/containers/Container.tsx
+++ src/components/ui/containers/Container.tsx
@@ -4,16 +4,18 @@ type ContainerProps = {
   children: ReactNode;
   hasBorderBottom?: boolean;
   as?: 'div' | 'section' | 'main' | 'footer' | 'header';
+  size?: 'max-w-screen-md' | 'max-w-screen-lg' | 'max-w-screen-xl';
 };
 
 export const Container: React.FC<ContainerProps> = ({
   children,
   hasBorderBottom = true,
   as: Element = 'div',
+  size = 'max-w-screen-lg',
 }) => {
   return (
     <Element
-      className={`max-w-screen-lg mx-auto px-4 md:px-8 py-12 md:py-15 xl:px-0 ${
+      className={`${size} mx-auto px-4 md:px-8 py-12 md:py-15 xl:px-0 ${
         hasBorderBottom && 'border-b-2 border-gray-dark/80'
       }`}
     >
