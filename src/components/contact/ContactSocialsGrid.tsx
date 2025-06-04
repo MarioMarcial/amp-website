5a366180c86145e8a2d663f6f8102e7242c7e9b0
Mario Marcial <mariomarciallp@gmail.com>
2025-06-03 22:32:13 -0600

    ContactSocialsGrid link href fixed

--- src/components/contact/ContactSocialsGrid.tsx
+++ src/components/contact/ContactSocialsGrid.tsx
@@ -12,7 +12,7 @@ export const ContactSocialsGrid = ({
       {socialsData.map((social) => (
         <a
           key={social.name}
-          href="https://facebook.com/academiamusica"
+          href={social.url}
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 py-2 px-4 rounded-lg bg-gray-dark hover:bg-[oklch(0.25_0.02_274.75)] transition-colors text-gray-200"
